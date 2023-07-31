42131ca36343a73c28697ab89eb26ebe05b44084
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-07-31 07:56:37 +0530

    demo6b

--- Demo6a.java
+++ Demo6a.java
@@ -1,6 +1,6 @@
 public class Demo6a {
     public static void main(String[] args) {
-        System.out.printf("\u001B[41m[     5\u001B[42m0      ]\u001B[49m[");
+        System.out.printf("\u001B[41m[     5\u001B[42m0     ]\u001B[49m[");
    
     }
 
---
+++ Demo6b.java
@@ -0,0 +1,16 @@
+public class Demo6b {
+public static void main(String[] args) {
+    System.out.printf("+-------------+----+\n");
+    System.out.printf("|\u001B[1m%-13s\u001B[0m|\u001B[1m%-4s\u001B[0m|\n", "NAME", "AGE");
+
+
+    System.out.printf("+-------------+----+\n");
+    System.out.printf("|%-13s|%-4s|\n", "Allice", "23");
+    System.out.printf("|%-13s|%-4s|\n", "Bob", "30");
+
+
+
+    System.out.printf("+-------------+----+\n");
+}
+
+}
