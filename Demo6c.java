5e0825ed593c7cf4143eeb67b7d01c780e0bfa2a
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-07-31 08:09:20 +0530

    demo6c

---
+++ Demo6c.java
@@ -0,0 +1,17 @@
+public class Demo6c{
+    public static void main(String[] args) {
+        System.out.printf("+-------------+------------+-------------+\n");
+        System.out.printf("|\u001B[1m%-13s\u001B[0m|\u001B[1m%-12s\u001B[0m|\u001B[1m%-13s\u001B[0m|\n", "CITY", "STATE", "POPULATION");
+
+
+        System.out.printf("+-------------+------------+-------------+\n");
+        System.out.printf("|%-13s|%-12s|%,13d|\n", "Los Angales", "California", 8523647);
+        System.out.printf("|%-13s|%-12s|%,13d|\n", "New York", "New York", 6987312);
+
+
+
+        System.out.printf("+-------------+------------+-------------+\n");
+    }
+
+
+}
