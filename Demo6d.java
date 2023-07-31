a03ec0b8ea069d736f5fa8212966e785e692b651
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-07-31 08:20:01 +0530

    demo6d

---
+++ Demo6d.java
@@ -0,0 +1,18 @@
+public class Demo6d {
+    public static void main(String[] args) {
+        System.out.printf("+----------+---------+------+\n");
+        System.out.printf("|\u001B[1m%10s\u001B[0m|\u001B[1m%9s\u001B[0m|\u001B[1m%6s\u001B[0m|\n", "ITEM", "QUANTITY", "PRICE");
+
+
+        System.out.printf("+----------+---------+------+\n");
+        System.out.printf("|%10s|%9s|%6s|\n", "Apples", "5", "$0.99");
+        System.out.printf("|%10s|%9s|%6s|\n", "Orranges", "10", "$1.49");
+
+
+
+        System.out.printf("+----------+---------+------+\n");
+    
+    }
+
+
+}
