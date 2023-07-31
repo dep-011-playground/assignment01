75692c002ebae56a741dd60bf231282391216c8f
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-07-31 07:09:38 +0530

    demo4

---
+++ Demo4.java
@@ -0,0 +1,6 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        System.out.printf("\u001B[31m%d\u001B[39m/\u001B[33m%d\u001B[39m/\u001B[30m%d \u001B[32m%d\u001B[39m:\u001B[36m%d\u001B[39m:\u001B[35m%d \n", 12, 28, 2023, 23,59,59);
+    }
+
+}
